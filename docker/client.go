006b86de959952d32d13bc6b4674212653162e5e
Christian Mäder <mail@cimnine.ch>
2017-09-22 17:48:41 +0200

    Finish the docker client things

--- docker/client.go
+++ docker/client.go
@@ -2,8 +2,6 @@ package docker
 
 import (
 	"fmt"
-	"time"
-
 	"strconv"
 
 	"github.com/fsouza/go-dockerclient"
@@ -24,7 +22,8 @@ type Container struct {
 type Client struct {
 	Endpoint     string
 	HostMap      *common.HostToHostMap
-	containerMap map[string]Container
+	containerMap *map[string]Container
+	dockerClient *docker.Client
 }
 
 // private_port.service.project.docker:public_port
@@ -40,31 +39,77 @@ func (client *Client) Launch() error {
 	if err != nil {
 		return err
 	}
+	client.dockerClient = dockerClient
 
-	containers, err := dockerClient.ListContainers(docker.ListContainersOptions{All: false})
+	containerMap := make(map[string]Container)
+	client.containerMap = &containerMap
+
+	client.updateMappings()
+
+	listener := make(chan *docker.APIEvents)
+	err = dockerClient.AddEventListener(listener)
 	if err != nil {
 		return err
 	}
 
-	buildContainerMap(containers, client)
+	defer func() {
+		err = dockerClient.RemoveEventListener(listener)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}()
 
-	if len(client.containerMap) == 0 {
-		return nil
+	for {
+		event := <-listener
+		err = client.handleEvent(event)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// re-reads the containers and re-builds the hostToHost Mapping
+func (client *Client) updateMappings() error {
+
+	err := client.updateContainers()
+	if err != nil {
+		return err
 	}
 
-	/*
-		Start of building Hostname Map
-	*/
+	err = client.updateHostMap()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// updates the map which maps containers names to their public ports
+func (client *Client) updateHostMap() error {
 	var mainProjectContainer, appProjectContainer *Container
 
 	client.HostMap.Lock()
 	defer client.HostMap.Unlock()
-	for _, container := range client.containerMap {
+
+	// clean the map
+	for key := range client.HostMap.M {
+		delete(client.HostMap.M, key)
+	}
+
+	containerMap := *client.containerMap
+
+	// add the appropriate names for any container to the map
+	for _, container := range containerMap {
+
 		if container.IsMain {
 			if mainProjectContainer == nil {
 				mainProjectContainer = &container
 			} else {
-				//TODO write warning: more than 1 "main"
+				fmt.Errorf(
+					"More than one container with 'reception.main' label, at least '%v' (chosen) and '%v' (ignoring).",
+					mainProjectContainer.Name,
+					container.Name,
+				)
 			}
 		}
 
@@ -78,12 +123,12 @@ func (client *Client) Launch() error {
 			backendHostname := fmt.Sprintf("%v:%v", port.IP, port.PublicPort)
 
 			frontendPortHostname := fmt.Sprintf(
-				"%v.%v.%v.docker:%v",
+				"%v.%v.%v.docker",
 				port.PrivatePort,
 				container.Service,
-				container.Project,
-				port.PublicPort)
+				container.Project)
 			client.HostMap.M[frontendPortHostname] = backendHostname
+			fmt.Printf("Added route from '%v' to '%v'.\n", frontendPortHostname, backendHostname)
 
 			// service.project.docker:public_port
 			if !mainServiceHostSet {
@@ -97,12 +142,12 @@ func (client *Client) Launch() error {
 				}
 
 				frontendServiceHostname := fmt.Sprintf(
-					"%v.%v.docker:%v",
+					"%v.%v.docker",
 					container.Service,
-					container.Project,
-					port.PublicPort)
-
+					container.Project)
 				client.HostMap.M[frontendServiceHostname] = backendHostname
+				fmt.Printf("Added route from '%v' to '%v'.\n", frontendServiceHostname, backendHostname)
+
 				mainServiceHostSet = true
 			}
 		}
@@ -142,74 +187,75 @@ func (client *Client) Launch() error {
 		}
 
 		frontendProjectHostname := fmt.Sprintf(
-			"%v.%v.docker:%v",
+			"%v.%v.docker",
 			container.Service,
-			container.Project,
-			thePort.PublicPort)
+			container.Project)
 		backendHostname := fmt.Sprintf("%v:%v", thePort.IP, thePort.PublicPort)
 		client.HostMap.M[frontendProjectHostname] = backendHostname
+		fmt.Printf("Added route from '%v' to '%v'.\n", frontendProjectHostname, backendHostname)
 	}
 
-	listener := make(chan *docker.APIEvents)
-	err = dockerClient.AddEventListener(listener)
+	return nil
+}
+
+// updates the list of containers
+func (client *Client) updateContainers() error {
+	containers, err := client.dockerClient.ListContainers(docker.ListContainersOptions{All: false})
 	if err != nil {
 		return err
 	}
 
-	defer func() {
-		err = dockerClient.RemoveEventListener(listener)
-		if err != nil {
-			fmt.Println(err)
-		}
-	}()
+	// clean old content
+	containerMap := *client.containerMap
+	for k := range containerMap {
+		delete(containerMap, k)
+	}
 
-	timeout := time.After(30 * time.Second)
-
-	for {
-		select {
-		case event := <-listener:
-			handleEvent(event)
-		case <-timeout:
-			return nil
-		}
+	// add all containers to the map
+	for _, container := range containers {
+		client.addToContainerMap(container)
 	}
 	return nil
 }
 
-func buildContainerMap(containers []docker.APIContainers, client *Client) {
-	for _, container := range containers {
-		if !hasPublicTcpPorts(container.Ports) {
-			continue
-		}
+// adds a container to the container map
+func (client *Client) addToContainerMap(container docker.APIContainers) {
+	if !hasPublicTcpPorts(container.Ports) {
+		return
+	}
 
-		_, ok := container.Labels["reception.main"]
-		httpPort, err := strconv.ParseInt(container.Labels["reception.http-port"], 10, 64)
-		if err != nil {
-			httpPort = 8080
-		}
+	_, ok := container.Labels["reception.main"]
+	httpPort, err := strconv.ParseInt(container.Labels["reception.http-port"], 10, 64)
+	if err != nil {
+		httpPort = 8080
+	}
 
-		client.containerMap[container.ID] = Container{
-			ID:              container.ID,
-			Name:            container.Names[0][1:],
-			PublicPorts:     filterPublicTcpPorts(container.Ports),
-			Project:         container.Labels["com.docker.compose.project"],
-			Service:         container.Labels["com.docker.compose.service"],
-			ContainerNumber: container.Labels["com.docker.compose.container-number"],
-			HttpPort:        httpPort,
-			IsMain:          ok,
-		}
+	publicTcpPorts := filterPublicTcpPorts(container.Ports)
+
+	containerMap := *client.containerMap
+	containerMap[container.ID] = Container{
+		ID:              container.ID,
+		Name:            container.Names[0][1:],
+		PublicPorts:     publicTcpPorts,
+		Project:         container.Labels["com.docker.compose.project"],
+		Service:         container.Labels["com.docker.compose.service"],
+		ContainerNumber: container.Labels["com.docker.compose.container-number"],
+		HttpPort:        httpPort,
+		IsMain:          ok,
 	}
 }
 
+// returns true, if any of the ports are exposed to the host machine
 func hasPublicTcpPorts(apiPorts []docker.APIPort) bool {
 	for _, port := range apiPorts {
-		if port.PublicPort != 0 && port.Type != "tcp" {
+		if port.PublicPort != 0 && port.Type == "tcp" {
 			return true
 		}
 	}
 	return false
 }
 
+// of the given ports it only returns those who are exposed to the host machine
 func filterPublicTcpPorts(apiPorts []docker.APIPort) (publicPorts []docker.APIPort) {
 	for _, port := range apiPorts {
 		if port.PublicPort == 0 || port.Type != "tcp" {
@@ -220,17 +266,19 @@ func filterPublicTcpPorts(apiPorts []docker.APIPort) (publicPorts []docker.APIPo
 	return
 }
 
-func handleEvent(event *docker.APIEvents) bool {
+// handles an event emitted by Docker
+func (client *Client) handleEvent(event *docker.APIEvents) error {
 	if event.Type != "container" {
-		return false
+		return nil
 	}
 
 	switch event.Action {
 	case "start", "stop":
-		fmt.Println("Action: ", event.Action)
-		fmt.Println("ID: ", event.ID)
-		return true
-	}
 
-	return false
+		err := client.updateMappings()
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
